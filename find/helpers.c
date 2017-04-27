3d883e17000acc6d67ce791d31249fc12e07fc7e
Ankit Maheshwari <infoankit130@gmail.com>
2017-04-27 11:09:27 +0000

    implemented Bubble sort i.e O(n^2) sorting algorithm

--- find/helpers.c
+++ find/helpers.c
@@ -28,5 +28,21 @@ bool search(int value, int values[], int n)
 void sort(int values[], int n)
 {
     // TODO: implement an O(n^2) sorting algorithm
+     int c;
+     do
+     { int b=0;
+       c=0;
+         for(int i = 0;i<(n-2);i++)
+         {   
+             if(values[i]>values[i+1])
+             {
+                b=values[i];
+                values[i]=values[i+1];
+                values[i+1]=b;
+                c++;
+                n--;
+             }
+         }
+     }while(c!=0);
     return;
 }
